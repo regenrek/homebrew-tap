5aa3667e6aef8fb97923027a6621b90880698d66
Kevin Kern <hello@regenrek.at>
2026-01-02 19:18:39 +0100

    brew: peakypanes v0.0.10

--- Formula/peakypanes.rb
+++ Formula/peakypanes.rb
@@ -1,8 +1,8 @@
 class Peakypanes < Formula
   desc "Terminal dashboard with YAML-based layouts and native live previews"
   homepage "https://github.com/regenrek/peakypanes"
-  url "https://github.com/regenrek/peakypanes/archive/refs/tags/v0.0.9.tar.gz"
-  sha256 "7355198c801b8ccd0da01e756e8a3fbba4d57fab9b4ceff5cfe3b104f954feb8"
+  url "https://github.com/regenrek/peakypanes/archive/refs/tags/v0.0.10.tar.gz"
+  sha256 "091f6f3b0f5cbeff674b0c34b2f977c8086237e99176f468260a79f4bb013bd4"
   depends_on "go" => :build
 
   def install
